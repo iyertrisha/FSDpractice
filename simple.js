3447a75239edd72c25716e198e98a5384164b329
Trisha <154061859+iyertrisha@users.noreply.github.com>
2025-12-17 19:42:02 +0000

    update

--- simple.js
+++ simple.js
@@ -14,15 +14,58 @@
     return res;
 }
 console.log(words("hello world"));*/
+/*
 const convert = () => {
-    const month = [January, Feb, march, april, may, june, july,
-                    august, september, october, november, december];
-    function monthnum(){
-        let res = Math.floor(month);
+    const month = ["January", "Feb", "march", "april", "may", "june", "july",
+                    "august", "september", "october", "november", "december"];
+    return function(num){
+        let res = Math.floor(num);
         if(isNaN(res) || res < 1 || res > 12){return 'BadNumber';}
-        else{return month[res-1]}
+        else{return month[res-1];}
     };
 };
-monthconversion = convert();
+const monthconversion = convert();
 console.log(monthconversion(2));
-console.log(monthconversion(17));
+console.log(monthconversion(17))
+important do not forget to use num for return fucntion, 2 return statements,*/
+/*
+function createStudent(usn, subjectCode, subjectName, cie, see) {
+let ciemarks = cie;
+let seemarks = see;
+
+return{
+    usn: usn,
+    subjectCode: subjectCode,
+    subjectName: subjectName,
+    getmarks: function(){
+        return ciemarks + seemarks;
+    },
+    display: function(){
+        console.log("usn: ",usn);
+        console.log("subjectcode: ",subjectCode);
+        console.log("subjectname; ",subjectName);
+        console.log("total marks: ",this.getmarks());
+    }
+};
+    
+};
+student1 = createStudent(
+    '1ms23cs001',
+    'cs52',
+    'computer netwroks',
+    '49',
+    '50'
+);
+student1.display();
+*/
+/*
+function plural(word, num){
+    if(num == 1){
+        return word + '';
+    }
+    else{
+        return word + 's';
+    }
+}
+console.log(plural('dog',5));
+*/
